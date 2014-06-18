c859b17e4130059af4756589f1dc245a6abdbaf5
Arthur Bikmullin <devolonter@gmail.com>
2014-06-18 22:51:29 +0400

    Build script refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,54 @@
+var ENVIRONMENT = "release";
+
 var gulp = require('gulp');
 var fs = require('fs');
 var exec = require('child_process').execFile;
 var path = require('path');
 var merge = require('merge-stream');
-
 var svg2png = require('gulp-svg2png');
 
-var CFG_FILE = 'config.develop';
-var HOST = '';
+var config = require('./buildconfig');
+var environment = config.environment[ENVIRONMENT];
+
+var host = '';
 
 if (process.platform == 'win32') {
-  HOST = 'winnt';
+  host = 'winnt';
 } else if (process.platform == 'linux') {
-  HOST = 'linux';
+  host = 'linux';
 } else {
-  HOST = 'macos';
+  host = 'macos';
 }
 
-CFG_FILE += '.' + HOST + '.txt';
-var TRANSCC = './bin/transcc_' + HOST;
-var TRANSCC_DEF_ARGS = ['-config=release', '-target=C++_Tool', '-builddir=.build', '-cfgfile=' + CFG_FILE]
+if (environment.transcc.config === '') {
+  environment.transcc.args = environment.transcc.args.concat('-cfgfile=config.' + environment.transcc.config + '.' + host + '.txt');
+}
 
-gulp.task('svg2png', function () {
-  gulp.src('./src/resources/logo/*.svg')
-    .pipe(svg2png())
-    .pipe(gulp.dest('./.output/png'));
-});
+if (config.path.transcc === '') {
+  config.path.transcc = './bin';
+}
+
+var transcc = config.path.transcc + '/transcc_' + host;
+var qmake = config.path.qt + '/qmake';
+
+if (process.platform == 'win32') {
+  transcc += '.exe';
+  qmake += '.exe';
+}
 
 gulp.task('transcc', function(callback) {
   var src = './src/transcc';
 
   return exec(
-    TRANSCC,
-    TRANSCC_DEF_ARGS.concat(path.resolve(src, 'transcc.monkey')),
+    transcc,
+    environment.transcc.args.concat(path.resolve(src, 'transcc.monkey')),
+
     function(err, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
 
-      var origin = 'main_' + HOST;
-      var dest = 'transcc_' + HOST;
+      var origin = 'main_' + host;
+      var dest = 'transcc_' + host;
 
       if (process.platform == 'win32') {
         origin += '.exe';
@@ -54,3 +64,9 @@ gulp.task('transcc', function(callback) {
     }
   );
 });
+
+/*gulp.task('svg2png', function () {
+ gulp.src('./src/resources/logo/*.svg')
+ .pipe(svg2png())
+ .pipe(gulp.dest('./.output/png'));
+ });*/
