50f58771168cb504bfe2b15204679c1dcb812a56
Arthur Bikmullin <devolonter@gmail.com>
2014-06-19 04:50:12 +0400

    Added task for dependencies, some improvements

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var fs = require('fs');
 var exec = require('child_process').execFile;
 var path = require('path');
+var wrench = require('wrench');
 var merge = require('merge-stream');
 var svg2png = require('gulp-svg2png');
 
@@ -20,7 +21,7 @@ if (process.platform == 'win32') {
   host = 'macos';
 }
 
-if (environment.transcc.config === '') {
+if (environment.transcc.config !== '') {
   environment.transcc.args = environment.transcc.args.concat('-cfgfile=config.' + environment.transcc.config + '.' + host + '.txt');
 }
 
@@ -31,6 +32,7 @@ if (config.path.transcc === '') {
 var transcc = config.path.transcc + '/transcc_' + host;
 var qmake = config.path.qt + '/qmake';
 var make = config.path.mingw + '/mingw32-make';
+var bin = './bin';
 
 if (process.platform == 'win32') {
   transcc += '.exe';
@@ -40,10 +42,15 @@ if (process.platform == 'win32') {
 
 gulp.task('transcc', function(callback) {
   var src = './src/transcc';
+  var buildDir = path.resolve(src, '.build');
+
+  if (fs.existsSync(buildDir)) {
+    wrench.rmdirSyncRecursive(buildDir);
+  }
 
   return exec(
     transcc,
-    environment.transcc.args.concat(path.resolve(src, 'transcc.monkey')),
+    environment.transcc.args.concat(["-builddir=.build", path.resolve(src, 'transcc.monkey')]),
 
     function(err, stdout, stderr) {
       console.log(stdout);
@@ -59,7 +66,7 @@ gulp.task('transcc', function(callback) {
 
       fs.renameSync(
         path.resolve(src, '.build/cpptool', origin),
-        path.resolve('./bin', dest)
+        path.resolve(bin, dest)
       );
 
       callback(err);
@@ -73,9 +80,12 @@ gulp.task('mserver', function(callback) {
 
   if (!fs.existsSync(buildDir)) {
     fs.mkdirSync(buildDir);
+  } else {
+    wrench.rmdirSyncRecursive(buildDir);
+    fs.mkdirSync(buildDir);
   }
 
-  return exec(
+  exec(
     qmake,
     environment.qmake.args.concat(path.resolve(src, 'mserver.pro')),
     {
@@ -92,6 +102,25 @@ gulp.task('mserver', function(callback) {
             console.log(stdout);
             console.log(stderr);
 
+            var origin = 'mserver';
+            var dest = 'mserver_' + host;
+
+            if (process.platform == 'win32') {
+              origin += '.exe';
+              dest += '.exe';
+            }
+
+            var tmp = path.resolve(buildDir, 'release', origin);
+
+            if (!fs.existsSync(tmp)) {
+              tmp = path.resolve(buildDir, 'debug', origin);
+            }
+
+            fs.renameSync(
+              tmp,
+              path.resolve(bin, dest)
+            );
+
             callback(err);
           }
         )
@@ -101,6 +130,25 @@ gulp.task('mserver', function(callback) {
     }
   );
 });
+
+gulp.task('dependencies', function(callback) {
+  environment.qt.dependencies.forEach(function(item) {
+    if (Array.isArray(item)) {
+      item.forEach(function(dep) {
+        fs.createReadStream(path.resolve(config.path.qt, dep)).pipe(fs.createWriteStream(path.resolve(bin, dep)));
+      });
+    } else {
+      wrench.copyDirRecursive(
+        path.resolve(config.path.qt, item),
+        path.resolve(bin, path.basename(item)),
+        {forceDelete: true},
+        callback
+      );
+    }
+  });
+});
+
+gulp.task('default', ['mserver', 'dependencies', 'transcc']);
 
 
 /*gulp.task('svg2png', function () {
