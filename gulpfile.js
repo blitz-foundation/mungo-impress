e31c991fa4da884e0d85286078e4a2c4787f46d6
Arthur Bikmullin <devolonter@gmail.com>
2014-07-03 02:29:10 +0400

    Added closure to build ignore

--- gulpfile.js
+++ gulpfile.js
@@ -323,7 +323,7 @@ gulp.task('dist', environment.options.build === 'clean' ? ['default'] : [], func
 
         if (file === 'gulpfile.js' || file === 'package.json' || file === 'bower.json' || file === 'settings.ini') {
           return false;
-        } else if (file === 'node_modules' || file === 'bower_components') {
+        } else if (file === 'node_modules' || file === 'bower_components' || file === 'closure') {
           return false;
         } else if (file.indexOf('.') === 0 || file.indexOf('.build') > 0) {
           return false;
